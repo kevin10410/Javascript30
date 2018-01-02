bc38500fb2e6d097911d0b093934fd1e79863f3b
wangyuhsiang <kevin10410@gmail.com>
2018-01-02 15:59:12 +0800

    modify

--- 19 - Webcam Fun/scripts-FINISHED.js
+++ 19 - Webcam Fun/scripts-FINISHED.js
@@ -40,8 +40,8 @@ function paintToCanvas() {
 
 function takePhoto() {
   // played the sound
-  snap.currentTime = 0;
-  snap.play();
+  // snap.currentTime = 0;
+  // snap.play();
 
   // take the data out of the canvas
   const data = canvas.toDataURL('image/jpeg');
